ee20a68073af8f4965afc0cdbe4c27f9114631b5
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-07-03 12:45:18 -0400

    Update globals.py

--- requient-his/globals.py
+++ requient-his/globals.py
@@ -21,6 +21,17 @@ class Vector:
  def __init__( self, x = 0.0 ):
   self.x = float( x )
 
+class Vector2:
+ def __init__( self, x= 0.0, y = 0.0 ):
+  self.x = float( x )
+  self.y = float( y )
+
+class Vector3:
+ def __init__( self, x=0.0, y=0.0, z=0.0 ):
+  self.x = float( x )
+  self.y = float( y )
+  self.z = float( z )
+
 def appDump( a : any ):
  del( a )
 
