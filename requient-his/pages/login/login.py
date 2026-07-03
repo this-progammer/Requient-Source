88efad8818d3baa8f9229709ef823664b6b9bc26
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-07-03 13:10:44 -0400

    Update login.py

--- requient-his/pages/login/login.py
+++ requient-his/pages/login/login.py
@@ -1,1 +1,28 @@
+"""*login.py*"""
+"""*july 3 2026*"""
 
+debug_kernel_gui()
+
+class Login:
+  def __init__( self, window ):
+    self.window = dkg().api.Window
+
+  def createLoginPage( self ):
+    return self.__init__( self, _PTR )
+
+  m_stringUsername = str
+  m_stringPassword = str
+  m_boolLoginCredintialsValid = bool
+
+  def getLoginPage( self ):
+    return self
+
+  def checkUserLogin( self ):
+    if self.m_stringUsername != True:
+      return assert( -1 )
+      
+    if self.m_stringPassword != True:
+      return assert( -1 )
+
+    else:
+      pageNextWindow()
